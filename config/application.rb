6b589c8075261a83a5fcbd37e64177fb7190ddf7
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2023-12-31 09:12:02 +0900

    設定ファイル

--- config/application.rb
+++ config/application.rb
@@ -10,18 +10,23 @@ module EzWeb
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+    # Rubyプロセス(JST)と同じにする
+    config.active_record.default_timezone = :local
+    config.autoload_paths += %W(#{Rails.root}/lib/utils)
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.test_framework :rspec,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false,
+      model_specs: false
+    end
   end
 end
